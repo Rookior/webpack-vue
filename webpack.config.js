26d9416c7b45096faaae5175542cee0c0ef7d231
= <1757629766@qq.com>
2020-11-06 14:41:06 +0800

    自动编译与热更新

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const CleanWebpackPlugin = require('clean-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
 module.exports = {
   entry: {
         app: './src/index.js',
-        print: './src/print.js'
   },
+  devServer: {
+      contentBase: './dist',
+      hot:true
+    },
+      module: {
+           rules: [
+             {
+               test: /\.css$/,
+               use: ['style-loader', 'css-loader']
+             }
+           ]
+         },
   plugins: [
         new CleanWebpackPlugin(),
         // new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: '管理输出'
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
   ],
   output: {
-    filename: 'bundle.js',
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   }
 };
