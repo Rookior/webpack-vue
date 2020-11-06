1897db9cf2d6fc7246da50dded68579625014d5f
= <1757629766@qq.com>
2020-11-06 10:21:03 +0800

    增加数据

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'loadsh';
 import './style.css';
 import Icon from './icon.png'
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
@@ -15,6 +16,7 @@ function component() {
     myIcon.src = Icon;
     element.appendChild(myIcon);
 
+    console.log(Data);
     return element;
   }
   
