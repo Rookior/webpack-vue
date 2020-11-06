a2305c67aabc6015dde570ba98b9bb64fe6e017b
= <1757629766@qq.com>
2020-11-06 11:23:00 +0800

    动态生成index.html与动态添加bundle

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import _ from 'loadsh';
-import './style.css';
-import Icon from './icon.png'
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    // Loadsh 通过这个js导入
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-     // 将图像添加到我们现有的 div。
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,22 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+  },
+  plugins: [
+        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+  ],
   output: {
     filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-         rules: [
-          {
-            //   加载css
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-           },
-           {
-               //加载图片
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                // 加载字体
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                   'file-loader'
-                ] 
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                'xml-loader'
-                ]
-            }
-         ]
-    }
+  }
 };
